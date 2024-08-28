a7b6fc2df23670b78db5664f8554e25c593a35a8
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-08-28 14:13:39 +0200

    fix: check for outsideClick on dialog

--- frontend/src/components/general/filter/Dialog.tsx
+++ frontend/src/components/general/filter/Dialog.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import FilterButton from '../buttons/FilterButton';
 import PrimaryButton from '../buttons/PrimaryButton';
 import SecondaryButton from '../buttons/SecondaryButton';
+import useOutsideClick from '@/hooks/useOutsideClick';
 
 interface DialogProps {
   headline: string;
@@ -15,13 +16,24 @@ const Dialog: React.FC<DialogProps> = ({ headline, children }) => {
     setIsOpen(!isOpen);
   };
 
+  // Use the useOutsideClick hook to detect clicks outside the dialog
+  const dialogRef = useOutsideClick(() => {
+    setIsOpen(false);
+  });
+
   return (
     <div>
+      {/* Overlay */}
       <div className={`bg-dark-900/90 fixed inset-0 z-50 ${isOpen ? 'block' : 'hidden'}`}></div>
+
+      {/* Trigger button */}
       <FilterButton ariaLabel={headline} onClick={handleFilterView} />
-      <section 
-        role="dialog" 
-        aria-modal="true" 
+
+      {/* Dialog content */}
+      <section
+        ref={dialogRef} // Attach the ref to the dialog section
+        role="dialog"
+        aria-modal="true"
         className={`fixed z-[60] inset-x-4 shadow-xl bg-white top-1/2 -translate-y-1/2 p-5 rounded-xl mx-auto max-w-[30rem]
           ${isOpen ? 'block' : 'hidden'}
       `}>
@@ -31,11 +43,11 @@ const Dialog: React.FC<DialogProps> = ({ headline, children }) => {
 
         <div className="flex flex-wrap gap-5">
           <PrimaryButton label="Anwenden" type="submit" />
-          <SecondaryButton label="Abbrechen" onClick={handleFilterView}/>
+          <SecondaryButton label="Abbrechen" onClick={handleFilterView} />
         </div>
       </section>
     </div>
   );
-}
+};
 
 export default Dialog;
