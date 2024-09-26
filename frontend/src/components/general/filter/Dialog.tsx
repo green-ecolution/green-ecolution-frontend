4fc1fe5745dfc5a8e6d15836a8386afb87406d58
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-26 12:46:11 +0200

    feat: get region from backend (#60)
    * feat: get regions from backend
    
    * chore: remove demo region

--- frontend/src/components/general/filter/Dialog.tsx
+++ frontend/src/components/general/filter/Dialog.tsx
@@ -6,8 +6,10 @@ import { X } from 'lucide-react';
 import useOutsideClick from '@/hooks/useOutsideClick';
 import { getWateringStatusDetails, WateringStatus } from '@/types/WateringStatus';
 import Option from './Option';
-import { Region } from '@/types/Region';
 import { useNavigate } from '@tanstack/react-router';
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { regionApi } from '@/api/backendApi';
+import { useAuthHeader } from '@/hooks/useAuthHeader';
 
 interface DialogProps {
   initStatusTags: string[];
@@ -21,6 +23,11 @@ const Dialog: React.FC<DialogProps> = ({ initStatusTags, initRegionTags, headlin
   const [isOpen, setIsOpen] = useState(false);
   const [statusTags, setStatusTags] = useState<string[]>(initStatusTags);
   const [regionTags, setRegionTags] = useState<string[]>(initRegionTags);
+  const authorization = useAuthHeader();
+  const { data: regionRes } = useSuspenseQuery({
+    queryKey: ['regions'],
+    queryFn: () => regionApi.v1RegionGet({ authorization }),
+  });
 
   const handleFilterView = () => setIsOpen(!isOpen);
   const dialogRef = useOutsideClick(() => setIsOpen(false));
@@ -106,17 +113,15 @@ const Dialog: React.FC<DialogProps> = ({ initStatusTags, initRegionTags, headlin
 
         <fieldset className="mt-6">
           <legend className="font-lato font-semibold text-dark-600 mb-2">Stadtteil in Flensburg:</legend>
-          {Object.entries(Region)
-            .filter(([key]) => key !== 'unknown')
-            .map(([regionKey, regionValue]) => (
-              <Option
-                key={regionKey}
-                label={regionValue}
-                name={regionKey}
-                checked={regionTags.includes(regionValue)}
-                onChange={handleFilterChange('region')}
-              />
-            ))}
+          {regionRes?.regions.map((region) => (
+            <Option
+              key={region.id}
+              label={region.name}
+              name={region.id}
+              checked={regionTags.includes(region.name)}
+              onChange={handleFilterChange('region')}
+            />
+          ))}
         </fieldset>
 
         <div className="flex flex-wrap gap-5 mt-6">
