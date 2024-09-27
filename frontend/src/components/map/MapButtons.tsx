2ad24b4d28f081b7e27e47d66e21ee60f3459ca5
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-27 16:19:42 +0200

    chore: remove tooltip

--- frontend/src/components/map/MapButtons.tsx
+++ frontend/src/components/map/MapButtons.tsx
@@ -1,9 +1,4 @@
 import { Settings, TreeDeciduous } from "lucide-react";
-import {
-  Tooltip,
-  TooltipContent,
-  TooltipTrigger,
-} from "@/components/ui/tooltip";
 import { Link } from "@tanstack/react-router";
 
 export interface MapButtonsProps extends React.PropsWithChildren {}
@@ -30,34 +25,24 @@ const DefaultMapButtons = () => {
 
 export const SettingsButton = () => {
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <button
-          className={`bg-white shadow-cards w-10 h-10 rounded-full flex items-center justify-center transition-all ease-in-out duration-300`}
-        >
-          <Settings className="w-6 h-6 text-dark-800" />
-        </button>
-      </TooltipTrigger>
-      <TooltipContent>Einstellungen</TooltipContent>
-    </Tooltip>
+    <button
+      className={`bg-white shadow-cards w-10 h-10 rounded-full flex items-center justify-center transition-all ease-in-out duration-300`}
+    >
+      <Settings className="w-6 h-6 text-dark-800" />
+    </button>
   );
 };
 
 export const AddTreeButton = () => {
   return (
-    <Tooltip>
-      <TooltipTrigger asChild>
-        <Link to="/map/tree/new" preload="intent" search={(prev) => prev}>
-          <button
-            className={`bg-white shadow-cards w-10 h-10 rounded-full flex items-center justify-center transition-all ease-in-out duration-300`}
-          >
-            {/* TODO: Find a better icon */}
-            <TreeDeciduous className="w-6 h-6 text-dark-800" />
-          </button>
-        </Link>
-      </TooltipTrigger>
-      <TooltipContent>Neuen Baum erfassen</TooltipContent>
-    </Tooltip>
+    <Link to="/map/tree/new" preload="intent" search={(prev) => prev}>
+      <button
+        className={`bg-white shadow-cards w-10 h-10 rounded-full flex items-center justify-center transition-all ease-in-out duration-300`}
+      >
+        {/* TODO: Find a better icon */}
+        <TreeDeciduous className="w-6 h-6 text-dark-800" />
+      </button>
+    </Link>
   );
 };
 
