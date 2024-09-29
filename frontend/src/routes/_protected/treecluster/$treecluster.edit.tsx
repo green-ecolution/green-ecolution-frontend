39675c0aab614067cb00b89aaec3f62ad5ce9cfb
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-09-29 22:33:09 +0200

    feat: edit treecluster form

--- frontend/src/routes/_protected/treecluster/$treecluster.edit.tsx
+++ frontend/src/routes/_protected/treecluster/$treecluster.edit.tsx
@@ -1,8 +1,14 @@
-import { clusterApi } from '@/api/backendApi';
+import { clusterApi, EntitiesTreeSoilCondition } from '@/api/backendApi';
 import LoadingInfo from '@/components/general/error/LoadingInfo';
+import FormForTreecluster from '@/components/general/form/FormForTreecluster';
 import { useAuthHeader } from '@/hooks/useAuthHeader';
+import { TreeclusterForm, TreeclusterSchema } from '@/schema/treeclusterSchema';
+import useStore from '@/store/store';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { useSuspenseQuery } from '@tanstack/react-query';
 import { createFileRoute, useLoaderData } from '@tanstack/react-router'
+import { useEffect, useMemo, useState } from 'react';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 export const Route = createFileRoute('/_protected/treecluster/$treecluster/edit')({
   component: EditTreeCluster,
@@ -13,13 +19,49 @@ export const Route = createFileRoute('/_protected/treecluster/$treecluster/edit'
 })
 
 function EditTreeCluster () {
-  const clusterId = useLoaderData({ from: '/_protected/treecluster/$treecluster/edit'});
   const authorization = useAuthHeader();
+  const clusterId = useLoaderData({ from: '/_protected/treecluster/$treecluster/edit'});
+  const newTreecluster = useStore((state) => state.newTreecluster);
+  const [displayError, setDisplayError] = useState(false);
 
   const { data: cluster, isLoading, isError } = useSuspenseQuery({
     queryKey: ["treecluster", clusterId],
     queryFn: () => clusterApi.getTreeClusterById({ clusterId, authorization }),
   });
+
+  const defaultValues = useMemo(() => ({
+    name: cluster?.name || '',
+    address: cluster?.address || '',
+    description: cluster?.description || '',
+    soilCondition: cluster?.soilCondition || EntitiesTreeSoilCondition.TreeSoilConditionUnknown,
+  }), [cluster]);
+
+  const { register, handleSubmit, formState: { errors }, getValues, reset } = useForm<TreeclusterForm>({
+    resolver: zodResolver(TreeclusterSchema()),
+    defaultValues,
+  });
+
+  useEffect(() => {
+    reset(defaultValues);
+  }, [defaultValues, reset]);
+
+  const onSubmit: SubmitHandler<TreeclusterForm> = async (data) => {
+    console.log(data);
+  };
+
+  const handleDeleteTree = (treeIdToDelete: number) => {
+    console.log(treeIdToDelete);
+    //newTreecluster.setTreeIds(newTreecluster.treeIds.filter((treeId) => treeId !== treeIdToDelete));
+  };
+
+  const storeState = () => {
+    const formData = getValues();
+    newTreecluster.setName(formData.name);
+    newTreecluster.setAddress(formData.address);
+    newTreecluster.setSoilCondition(formData.soilCondition);
+    newTreecluster.setDescription(formData.description);
+  };
+  
 
   return (
     <div className="container mt-6">
@@ -42,7 +84,15 @@ function EditTreeCluster () {
           </article>
 
           <section className="mt-10">
-
+          <FormForTreecluster
+            register={register}
+            handleDeleteTree={handleDeleteTree}
+            handleSubmit={handleSubmit}
+            displayError={displayError}
+            errors={errors}
+            onSubmit={onSubmit}
+            treeIds={cluster?.trees.map(tree => tree.id)} 
+            storeState={storeState} />
           </section>
         </div>
       )}
