a9360290d24f3a205d851008925b7f41736c37d5
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-09-30 23:38:50 +0200

    feat: add lib to frontend and implement plugin view

---
+++ frontend/src/routes/_protected/settings/index.tsx
@@ -0,0 +1,53 @@
+import DashboardCard from "@/components/general/cards/DashboardCard";
+import { createFileRoute } from "@tanstack/react-router";
+
+export const Route = createFileRoute("/_protected/settings/")({
+  component: Settings,
+});
+
+const cards = [
+  {
+    url: "/settings/plugin",
+    description: "Alle Plugins, die in der Anwendung installiert sind.",
+    headline: "Plugins",
+    linkLabel: "Zu den Plugins",
+  },
+];
+
+function Settings() {
+  return (
+    <div className="container mt-6">
+      <article className="mb-10 2xl:w-4/5">
+        <h1 className="font-lato font-bold text-3xl mb-4 lg:text-4xl xl:text-5xl">
+          Ihre persönlichen Einstellungen
+        </h1>
+        <p>
+          Eu ipsum occaecat non exercitation occaecat ea aute fugiat quis magna
+          do veniam commodo. Magna Lorem cupidatat id fugiat nostrud quis qui in
+          quis fugiat. Irure pariatur anim cupidatat nulla ipsum Lorem irure.
+          Est elit laborum sunt commodo officia nulla cupidatat fugiat tempor
+          exercitation laborum. Sint irure eiusmod sunt. Magna esse proident
+          magna dolore aliqua nulla id sunt adipisicing.
+        </p>
+      </article>
+
+      <h2 className="text-sm font-semibold text-dark-800 mb-4">
+        Schnellverweise
+      </h2>
+
+      <ul className="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
+        {cards.map((card, key) => (
+          <li key={key}>
+            <DashboardCard
+              headline={card.headline}
+              description={card.description}
+              linkLabel={card.linkLabel}
+              url={card.url}
+              isDark={key % 2 !== 0}
+            />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
