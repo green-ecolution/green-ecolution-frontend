1e3bf834b04e2b62757dede938e5bfa88094aa42
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-03 17:33:07 +0200

    v0.0.1

---
+++ csv_import/lib/components/context.tsx
@@ -0,0 +1,30 @@
+import { createContext, useContext } from "react";
+
+export interface PluginContext {
+  authToken: string;
+}
+
+export const PluginContext = createContext<PluginContext | undefined>(
+  undefined,
+);
+
+export interface PluginProviderProps extends React.PropsWithChildren {
+  authToken: string;
+}
+
+export const PluginProvider = ({ authToken, children }: PluginProviderProps) => {
+  return (
+    <PluginContext.Provider value={{ authToken }}>
+      {children}
+    </PluginContext.Provider>
+  );
+};
+
+export const usePluginContext = () => {
+  const context = useContext(PluginContext);
+  if (context === undefined) {
+    throw new Error("usePluginContext must be used within a PluginProvider");
+  }
+  return context;
+};
+
