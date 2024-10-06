a8dc0db18367ddf5807fd84db2e0179c7fed6f70
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-07 01:50:06 +0200

    chore: remove unused zustand middleware

--- frontend/src/store/form/useFormStore.ts
+++ frontend/src/store/form/useFormStore.ts
@@ -1,7 +1,6 @@
 import { create } from "zustand";
 import { devtools } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
-import { subscribeWithSelector } from "zustand/middleware";
 import { DeepPartial } from "react-hook-form";
 
 type FormStoreState<Form> = {
@@ -20,33 +19,31 @@ export type FormStore<T> = FormStoreState<T> & FormStoreActions<T>;
 
 const useFormStore = create<FormStore<any>>()(
   devtools(
-    immer(
-      subscribeWithSelector((set, get) => ({
-        form: undefined,
-        type: "new",
-        commit: (form) => {
-          console.log("commit", form);
-          set((state) => {
-            state.form = form;
-          });
-        },
-        reset: () => {
-          console.log("reset before", get().form);
-          set((state) => {
-            state.form = undefined;
-          });
-          console.log("reset after", get().form);
-        },
-        isEmpty: () => {
-          return get().form === undefined;
-        },
-        setType: (type) => {
-          set((state) => {
-            state.type = type;
-          });
-        },
-      })),
-    ),
+    immer((set, get) => ({
+      form: undefined,
+      type: "new",
+      commit: (form) => {
+        console.log("commit", form);
+        set((state) => {
+          state.form = form;
+        });
+      },
+      reset: () => {
+        console.log("reset before", get().form);
+        set((state) => {
+          state.form = undefined;
+        });
+        console.log("reset after", get().form);
+      },
+      isEmpty: () => {
+        return get().form === undefined;
+      },
+      setType: (type) => {
+        set((state) => {
+          state.type = type;
+        });
+      },
+    })),
   ),
 );
 
