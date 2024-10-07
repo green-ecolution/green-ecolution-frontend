12998eac0c7853ab35c38b7edeb582e54af0caa8
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-07 17:33:04 +0200

    chore: use props prefix

--- frontend/src/components/general/form/types/Textarea.tsx
+++ frontend/src/components/general/form/types/Textarea.tsx
@@ -8,14 +8,13 @@ interface TextareaProps
 
 const Textarea = forwardRef(
   (props: TextareaProps, ref: React.ForwardedRef<HTMLTextAreaElement>) => {
-    const { name, label, required, error } = props;
     return (
       <div>
         <label
-          htmlFor={name}
+          htmlFor={props.name}
           className="block font-semibold text-dark-800 mb-2.5"
         >
-          {label} {required ? <span className="text-red">*</span> : null}
+          {props.label} {props.required ? <span className="text-red">*</span> : null}
         </label>
         <textarea
           ref={ref}
@@ -24,8 +23,8 @@ const Textarea = forwardRef(
           className="w-full text-dark-800 border border-green-light rounded-lg bg-white px-4 py-3 focus:outline-green-dark"
           {...props}
         />
-        {error && (
-          <span className="text-red mt-2 font-semibold text-sm">{error}</span>
+        {props.error && (
+          <span className="text-red mt-2 font-semibold text-sm">{props.error}</span>
         )}
       </div>
     );
