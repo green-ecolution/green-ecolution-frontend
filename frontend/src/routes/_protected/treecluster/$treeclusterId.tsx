66dbe623dd401cbe42ca52c0567f8ae175c7d1b9
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-12 22:22:30 +0200

    feat: refactoring tree cluster dashboard

--- frontend/src/routes/_protected/treecluster/$treeclusterId.tsx
+++ frontend/src/routes/_protected/treecluster/$treeclusterId.tsx
@@ -1,105 +1,32 @@
-import { treeClusterIdQuery } from '@/api/queries'
-import queryClient from '@/api/queryClient'
-import EntitiesStatusCard from '@/components/general/cards/EntitiesStatusCard'
-import GeneralStatusCard from '@/components/general/cards/GeneralStatusCard'
-import TreeCard from '@/components/general/cards/TreeCard'
-import BackLink from '@/components/general/links/BackLink'
-import ButtonLink from '@/components/general/links/ButtonLink'
-import { getWateringStatusDetails } from '@/hooks/useDetailsForWateringStatus'
-import { useSuspenseQuery } from '@tanstack/react-query'
+import LoadingInfo from '@/components/general/error/LoadingInfo'
+import TreeClusterDashboard from '@/components/treecluster/TreeClusterDashboard'
 import { createFileRoute } from '@tanstack/react-router'
-import { Pencil } from 'lucide-react'
+import { Suspense } from 'react'
+import { ErrorBoundary } from 'react-error-boundary'
 
 export const Route = createFileRoute('/_protected/treecluster/$treeclusterId')({
   component: SingleTreecluster,
-  loader: async ({ params }) => {
-    return queryClient.ensureQueryData(treeClusterIdQuery(params.treeclusterId))
-  },
 })
 
 function SingleTreecluster() {
   const clusterId = Route.useParams().treeclusterId
-  const { data: treecluster } = useSuspenseQuery(treeClusterIdQuery(clusterId))
-  const wateringStatus = getWateringStatusDetails(treecluster.wateringStatus)
-
   return (
     <div className="container mt-6">
-      <BackLink
-        link={{ to: '/treecluster' }}
-        label="Zu allen Bewässerungsgruppen"
-      />
-      <article className="space-y-6 2xl:space-y-0 2xl:flex 2xl:items-center 2xl:space-x-10">
-        <div className="2xl:w-4/5">
-          <h1 className="font-lato font-bold text-3xl mb-4 lg:text-4xl xl:text-5xl">
-            Bewässerungsgruppe: {treecluster.name}
-          </h1>
-          <p>{treecluster.description}</p>
-        </div>
-        <ButtonLink
-          icon={Pencil}
-          iconClassName="stroke-1"
-          label="Gruppe bearbeiten"
-          color="grey"
-          link={{
-            to: '/treecluster/$treecluster/edit',
-            params: { treecluster: clusterId },
-          }}
-        />
-      </article>
-
-      <section className="mt-10">
-        <ul className="space-y-5 md:space-y-0 md:grid md:gap-5 md:grid-cols-2 lg:grid-cols-3">
-          <li>
-            <EntitiesStatusCard
-              statusDetails={wateringStatus}
-              label="Bewässerungszustand (ø)"
-            />
-          </li>
-          <li>
-            <GeneralStatusCard
-              overline="Baumanzahl in der Gruppe"
-              value={
-                treecluster.trees?.length
-                  ? `${treecluster.trees.length} ${treecluster.trees.length > 1 ? 'Bäume' : 'Baum'}`
-                  : 'Keine Bäume'
-              }
-              description="Nicht alle Bäume haben Sensoren, da Rückschlüsse möglich sind."
-            />
-          </li>
-          <li>
-            <GeneralStatusCard
-              overline="Standort der Gruppe"
-              value={`${treecluster.address}, ${treecluster.region?.name ?? '-'}`}
-            />
-          </li>
-        </ul>
-      </section>
-
-      <section className="mt-16">
-        <h2 className="text-xl font-bold font-lato mb-10">
-          Alle zugehörigen Bäume
-        </h2>
-
-        <header className="hidden border-b pb-2 text-sm text-dark-800 px-6 border-b-dark-200 mb-5 lg:grid lg:grid-cols-[1fr,2fr,1fr,1fr] lg:gap-5">
-          <p>Status</p>
-          <p>Baumart</p>
-          <p>Baumnummer</p>
-        </header>
-
-        <ul className="space-y-5">
-          {treecluster.trees?.length === 0 ? (
-            <li className="text-center text-dark-600 mt-4">
-              <p>Der Bewässerungsgruppe wurden keine Bäume hinzugefügt.</p>
-            </li>
-          ) : (
-            treecluster.trees?.map((tree, key) => (
-              <li key={key}>
-                <TreeCard tree={tree} />
-              </li>
-            ))
-          )}
-        </ul>
-      </section>
+      <Suspense
+        fallback={<LoadingInfo label="Bewässerungsgruppe wird geladen …" />}
+      >
+        <ErrorBoundary
+          fallback={
+            <p className="text-red text-lg">
+              Eine Bewässerungsgruppe mit der Identifikationsnummer {clusterId}{' '}
+              gibt es nicht oder die Bewässerungsgruppendaten konnten nicht
+              geladen werden
+            </p>
+          }
+        >
+          <TreeClusterDashboard clusterId={clusterId} />
+        </ErrorBoundary>
+      </Suspense>
     </div>
   )
 }
