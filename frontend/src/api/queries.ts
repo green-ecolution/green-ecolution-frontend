b7f070f0e25d9896fbde1c15bef9a3806d0ed419
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-13 12:27:34 +0200

    fix: auth header access token is undefind

--- frontend/src/api/queries.ts
+++ frontend/src/api/queries.ts
@@ -14,16 +14,16 @@ import {
 } from './backendApi'
 import useStore from '@/store/store'
 
-const authHeader = {
+const authHeader = () => ({
   authorization: `Bearer ${useStore.getState().auth.token?.accessToken}`,
-}
+})
 
 export const treeClusterQuery = () =>
   queryOptions<TreeClusterList>({
     queryKey: ['cluster'],
     queryFn: () =>
       clusterApi.getAllTreeClusters({
-        authorization: authHeader.authorization,
+        authorization: authHeader().authorization,
       }),
   })
 
@@ -32,7 +32,7 @@ export const treeClusterIdQuery = (id: string) =>
     queryKey: ['treescluster', id],
     queryFn: () =>
       clusterApi.getTreeClusterById({
-        authorization: authHeader.authorization,
+        authorization: authHeader().authorization,
         clusterId: id,
       }),
   })
@@ -41,32 +41,35 @@ export const sensorQuery = () =>
   queryOptions<SensorList>({
     queryKey: ['sensors'],
     queryFn: () =>
-      sensorApi.getAllSensors({ authorization: authHeader.authorization }),
+      sensorApi.getAllSensors({ authorization: authHeader().authorization }),
   })
 
 export const treeQuery = () =>
   queryOptions<TreeList>({
     queryKey: ['tree'],
     queryFn: () =>
-      treeApi.getAllTrees({ authorization: authHeader.authorization }),
+      treeApi.getAllTrees({ authorization: authHeader().authorization }),
   })
 
 export const treeIdQuery = (id: string) =>
   queryOptions<Tree>({
     queryKey: ['tree', id],
     queryFn: () =>
-      treeApi.getTrees({ treeId: id, authorization: authHeader.authorization }),
+      treeApi.getTrees({
+        treeId: id,
+        authorization: authHeader().authorization,
+      }),
   })
 export const regionsQuery = () =>
   queryOptions({
     queryKey: ['regions'],
     queryFn: () =>
-      regionApi.v1RegionGet({ authorization: authHeader.authorization }),
+      regionApi.v1RegionGet({ authorization: authHeader().authorization }),
   })
 
 export const infoQuery = () =>
   queryOptions<AppInfo>({
     queryKey: ['info'],
     queryFn: () =>
-      infoApi.getAppInfo({ authorization: authHeader.authorization }),
+      infoApi.getAppInfo({ authorization: authHeader().authorization }),
   })
