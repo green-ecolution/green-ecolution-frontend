30b1b9284da033e0a45c78b71eecabaafa61c2f1
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-13 15:00:10 +0200

    feat: bring back the import card in settings

--- frontend/src/routes/_protected/settings.tsx
+++ frontend/src/routes/_protected/settings.tsx
@@ -1,7 +1,7 @@
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/_protected/settings")({
-  component: Settings,
+  component: () => <Outlet />,
   meta: () => [
     {
       title: 'Einstellungen',
@@ -10,20 +10,3 @@ export const Route = createFileRoute("/_protected/settings")({
   ],
 });
 
-function Settings() {
-  return (
-    <div className="container mt-6">
-      <article className="2xl:w-4/5">
-        <h1 className="font-lato font-bold text-3xl mb-4 lg:text-4xl xl:text-5xl">
-          Ihre persönlichen Einstellungen
-        </h1>
-        <p>
-          Eu ipsum occaecat non exercitation occaecat ea aute fugiat quis magna do veniam commodo.
-          Magna Lorem cupidatat id fugiat nostrud quis qui in quis fugiat. Irure pariatur anim cupidatat nulla ipsum Lorem irure. 
-          Est elit laborum sunt commodo officia nulla cupidatat fugiat tempor exercitation laborum. Sint irure eiusmod sunt. 
-          Magna esse proident magna dolore aliqua nulla id sunt adipisicing.
-        </p>
-      </article>
-    </div>
-  );
-}
