0adf02a300e3e3df681d65ad43f83e4bde57cb17
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-16 23:01:26 +0200

    feat: refresh token on root

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,8 +1,22 @@
+import { userApi } from '@/api/backendApi'
 import App from '@/App'
+import useStore from '@/store/store'
 import { createRootRoute } from '@tanstack/react-router'
 
 export const Route = createRootRoute({
   component: Root,
+  beforeLoad: async () => {
+    const token = await userApi.v1UserTokenRefreshPost({
+      body: {
+        refreshToken: useStore.getState().auth.token?.refreshToken || '',
+      },
+    })
+    if (!token) {
+      return
+    }
+    useStore.getState().auth.setToken(token)
+    useStore.getState().user.setFromJwt(token.accessToken)
+  }
 })
 
 function Root() {
