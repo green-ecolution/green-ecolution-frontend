b1e3f649c72560f381f3f77542381c2cf4358362
Cedrik Hoffmann <cedrik.hoffmann@jgdperl.com>
2024-10-16 22:42:05 +0200

    chore: change localhost to 127.0.0.1

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     host: true,
     proxy: {
       "/api-local": {
-        target: "http://localhost:3000",
+        target: "http://127.0.0.1:3000",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-local/, "/api"),
       },
