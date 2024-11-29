cebc4ab9ddadcea77c2693bcdb935557d987b8e7
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-11-29 11:16:17 +0100

    feat: add gap between icon and prevent icon from shrinking

--- frontend/src/components/treecluster/TreeClusterDashboard.tsx
+++ frontend/src/components/treecluster/TreeClusterDashboard.tsx
@@ -31,14 +31,14 @@ const TreeClusterDashboard = ({ clusterId }: TreeClusterDashboardProps) => {
             Bewässerungsgruppe: {treecluster.name}
           </h1>
           <p className="mb-4">{treecluster.description}</p>
-          {treecluster.trees?.length > 0 ? (
+          {treecluster.trees?.length ? (
             <GeneralLink
               url={`/map?lat=${treecluster.latitude}&lng=${treecluster.longitude}&zoom=16&cluster=${treecluster.id}`}
               label="Auf der Karte anzeigen"
             />
           ) : (
-            <div className="flex items-center">
-              <TriangleAlert className="text-dark-600 w-5 h-5" />
+            <div className="flex items-center gap-x-2">
+              <TriangleAlert className="flex-shrink-0 text-dark-600 w-5 h-5" />
               <p className="ml-2 text-dark-600">
                 Diese Baumgruppe enthält keine Bäume und hat daher keinen Standort.
               </p>
