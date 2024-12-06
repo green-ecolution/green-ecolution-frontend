e4bb7438f4a602696c495a87919e5631ce95c3ef
Sophia Anthopoulos <sophia.anthopoulos@gmail.com>
2024-12-06 13:30:35 +0100

    fix: used Pill Component

--- frontend/src/components/general/cards/VehicleCard.tsx
+++ frontend/src/components/general/cards/VehicleCard.tsx
@@ -16,16 +16,23 @@ const VehicleCard: React.FC<VehicleCard> = ({ vehicle }) => {
     trailer: 'Anhänger',
   }
 
+  const pillTypeMapping = {
+    "Unbekannt": 'dark-400',
+    "Nicht verfügbar": 'red',
+    "Verfügbar":  'green-light',
+    "Im Einsatz": 'active',
+  }
+
   return (
     <Link
       to={`/vehicle/${vehicle.id}`}
       className="bg-white border border-dark-50 p-6 rounded-xl shadow-cards flex flex-col gap-y-4 transition-all ease-in-out duration-300 hover:bg-green-dark-50 hover:border-green-dark lg:grid lg:grid-cols-5 lg:items-center lg:gap-5 lg:py-10 xl:px-10"
     >
-      <Pill label={statusDetails.label}></Pill>
+      <Pill label={statusDetails.label} theme={pillTypeMapping[statusDetails.label] || 'dark-400'}></Pill>
 
       <div>
       <span className="lg:sr-only">Kennzeichen: </span>
-        <h2 className="font-bold text-lg mb-0.5">{vehicle.numberPlate}</h2>
+        <h2 className="text-dark-600 font-bold text-lg mb-0.5">{vehicle.numberPlate}</h2>
         <p className="text-dark-600 lg:block lg:text-sm">
           {vehicleTypeMapping[vehicle.type] || vehicle.type}
         </p>
