f1d77b6e1966c3c8f1f6bac028e12a4f38129b9a
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-08 18:53:27 +0100

    refactor: simplify code

--- frontend/src/routes/_protected/map/sensor/select.tree.tsx
+++ frontend/src/routes/_protected/map/sensor/select.tree.tsx
@@ -1,19 +1,17 @@
 import SelectedCard from '@/components/general/cards/SelectedCard'
 import MapSelectTreesModal from '@/components/map/MapSelectTreesModal'
 import WithAllTrees from '@/components/map/marker/WithAllTrees'
+import { Tree } from '@green-ecolution/backend-client'
 import { createFileRoute, useNavigate } from '@tanstack/react-router'
 import { useCallback, useState } from 'react'
 
-export const Route = createFileRoute('/_protected/map/sensor/select/tree')(
-  {
-    component: LinkTreeToSensor,
-    meta: () => [{ title: 'Vegetation verlinken' }],
-  }
-)
-
+export const Route = createFileRoute('/_protected/map/sensor/select/tree')({
+  component: LinkTreeToSensor,
+  meta: () => [{ title: 'Vegetation verlinken' }],
+})
 
 function LinkTreeToSensor() {
-  const [treeIds, setTreeIds] = useState<number[]>()
+  const [treeId, setTreeId] = useState<number | undefined>()
   const [showError, setShowError] = useState(false)
   const navigate = useNavigate({ from: Route.fullPath })
   const { sensorId } = Route.useSearch()
@@ -24,57 +22,36 @@ function LinkTreeToSensor() {
       params: { sensorId: sensorId?.toString() ?? '' },
       search: { resetStore: false },
     })
-   }, [navigate, sensorId])
+  }, [navigate, sensorId])
 
   const handleSave = () => {
-    if (treeIds.length === 0) {
+    if (!treeId) {
       setShowError(true)
       return
     }
     handleNavigateBack()
   }
 
-  const handleCancel = () => handleNavigateBack()
-
-  const handleDeleteTree = (treeId: number) => {
-    setTreeIds((prev) => prev.filter((id) => id !== treeId))
-  }
-
-  const handleTreeClick = (tree: Tree) => {
-    if (treeIds.includes(tree.id)) {
-      setTreeIds((prev) => prev.filter((id) => id !== tree.id))
-    } else {
-      setTreeIds((prev) => [...prev, tree.id])
-    }
-  }
-
   return (
     <>
       <MapSelectTreesModal
         onSave={handleSave}
-        onCancel={handleCancel}
-        disabled={treeIds.length === 0}
+        onCancel={() => handleNavigateBack()}
+        disabled={!treeId}
         title="Ausgewählte Bäume:"
         content={
           <ul className="space-y-3">
-            {(treeIds?.length || 0) === 0 || showError ? (
+            {!treeId || showError ? (
               <li className="text-dark-600 font-semibold text-sm">
                 <p>Hier können Sie zugehörige Bäume verlinken.</p>
               </li>
             ) : (
-              treeIds.map((treeId, key) => (
-                <li key={key}>
-                  <SelectedCard treeId={treeId} onClick={handleDeleteTree} />
-                </li>
-              ))
+              <SelectedCard treeId={treeId} onClick={() => setTreeId(undefined)} />
             )}
           </ul>
         }
       />
-      <WithAllTrees
-        selectedTrees={treeIds}
-        onClick={handleTreeClick}
-      />
+      <WithAllTrees selectedTrees={[]} onClick={(tree: Tree) => setTreeId(tree.id)} />
     </>
   )
 }
