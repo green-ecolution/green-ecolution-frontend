fec116ab0ce0e11c5b2c4ee7a8b0813d69751612
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-16 11:09:56 +0100

    feat: update watering plan card

--- frontend/src/routes/_protected/watering-plans/index.tsx
+++ frontend/src/routes/_protected/watering-plans/index.tsx
@@ -1,6 +1,7 @@
-import { wateringPlanQuery } from '@/api/queries'
+import { treeClusterIdQuery, vehicleIdQuery } from '@/api/queries'
 import WateringPlanCard from '@/components/general/cards/WateringPlanCard'
 import LoadingInfo from '@/components/general/error/LoadingInfo'
+import { WateringPlanStatus } from '@green-ecolution/backend-client'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Suspense } from 'react'
@@ -11,7 +12,27 @@ export const Route = createFileRoute('/_protected/watering-plans/')({
 })
 
 function WateringPlans() {
-  const { data: wateringPlanRes } = useSuspenseQuery(wateringPlanQuery())
+  //const { data: wateringPlanRes } = useSuspenseQuery(wateringPlanQuery())
+  const { data: trailer } = useSuspenseQuery(vehicleIdQuery("1"))
+  const { data: transporter } = useSuspenseQuery(vehicleIdQuery("2"))
+  const { data: cluster } = useSuspenseQuery(treeClusterIdQuery("1"))
+  const exampleDate = new Date();
+  
+
+  const exampleWateringPlan = {
+    id: 1,
+    date: "10.08.2000",
+    createdAt: exampleDate,
+    updatedAt: exampleDate,
+    description: "Dies ist eine Beschreibung",
+    wateringPlanStatus: WateringPlanStatus.WateringPlanStatusPlanned,
+    distance: 1000,
+    totalWaterRequired: 1000,
+    users: [],
+    treeCluster: [cluster],
+    transporter: transporter,
+    trailer: trailer,
+  }
 
   return (
     <div className="container mt-6">
@@ -26,12 +47,12 @@ function WateringPlans() {
       </article>
 
       <section className="mt-10">
-        <header className="hidden border-b pb-2 text-sm text-dark-800 px-8 border-b-dark-200 mb-5 lg:grid lg:grid-cols-5 lg:gap-5 xl:px-10">
+        <header className="hidden border-b pb-2 text-sm text-dark-800 px-8 border-b-dark-200 mb-5 lg:grid lg:grid-cols-[1fr,1.5fr,1fr,1.5fr,1.5fr] lg:gap-5 xl:px-10">
           <p>Status</p>
-          <p>Kennzeichen</p>
-          <p>Wasserkapazität</p>
-          <p>Modell</p>
-          <p>Führerscheinklasse</p>
+          <p>Datum & Fahrzeug</p>
+          <p>Länge</p>
+          <p>Anzahl Mitarbeitenden</p>
+          <p>Anzahl Bewässerungsgruppen</p>
         </header>
         <Suspense fallback={<LoadingInfo label="Daten werden geladen" />}>
           <ErrorBoundary
@@ -43,17 +64,9 @@ function WateringPlans() {
             }
           >
             <ul>
-              {wateringPlanRes.data?.length === 0 ? (
-                <li className="text-center text-dark-600 mt-10">
-                  <p>Es wurden leider keine Fahrzeuge gefunden.</p>
-                </li>
-              ) : (
-                wateringPlanRes.data?.map((wateringPlan, key) => (
-                  <li key={key} className="mb-5 last:mb-0">
-                    <WateringPlanCard wateringPlan={wateringPlan} />
-                  </li>
-                ))
-              )}
+              <li className="mb-5 last:mb-0">
+                <WateringPlanCard wateringPlan={exampleWateringPlan} />
+              </li>
             </ul>
           </ErrorBoundary>
         </Suspense>
