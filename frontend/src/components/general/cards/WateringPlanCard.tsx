3d25a21a9b7b1ffd4b8faa059ebea191ab4aff91
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-16 11:09:56 +0100

    feat: update watering plan card

--- frontend/src/components/general/cards/WateringPlanCard.tsx
+++ frontend/src/components/general/cards/WateringPlanCard.tsx
@@ -7,38 +7,39 @@ interface WateringPlanCard {
 }
 
 const WateringPlanCard: React.FC<WateringPlanCard> = ({ wateringPlan }) => {
-  // const statusDetails = getVehicleStatusDetails(vehicle.status)
-  // const vehicleType = getVehicleType(vehicle.type)
 
   return (
     <Link
       to={`/watering-plans/${wateringPlan.id}`}
-      className="bg-white border border-dark-50 p-6 rounded-xl shadow-cards flex flex-col gap-y-4 transition-all ease-in-out duration-300 hover:bg-green-dark-50 hover:border-green-dark lg:grid lg:grid-cols-5 lg:items-center lg:gap-5 lg:py-10 xl:px-10"
+      className="bg-white border border-dark-50 p-6 rounded-xl shadow-cards flex flex-col gap-y-4 transition-all ease-in-out duration-300 hover:bg-green-dark-50 hover:border-green-dark lg:grid lg:grid-cols-[1fr,1.5fr,1fr,1.5fr,1.5fr] lg:items-center lg:gap-5 lg:py-10 xl:px-10"
     >
       {/* <Pill label={statusDetails.label} theme={statusDetails.color}></Pill> */}
       <p>Test</p>
-      {/* <div>
+      <div>
         <h2 className="text-dark font-bold text-lg mb-0.5">
-          <span className="lg:sr-only">Kennzeichen: </span>
-          {wateringPlan.}
+          <span className="lg:sr-only">Einsatzplan: </span>
+          {wateringPlan.date}
         </h2>
-        <p className="text-dark-600 lg:text-sm">{vehicleType}</p>
+        <p className="text-dark-600 lg:text-sm">
+          {wateringPlan.transporter.numberPlate}
+          {wateringPlan.trailer && <span> | {wateringPlan.trailer.numberPlate}</span>}
+        </p>
       </div>
 
       <p className="text-dark-800">
-        <span className="lg:sr-only">Wasserkapazität:&nbsp;</span>
-        {vehicle.waterCapacity} Liter
+        <span className="lg:sr-only">Länge:&nbsp;</span>
+        {wateringPlan.distance} km
       </p>
 
       <p className="text-dark-800">
-        <span className="lg:sr-only">Modell:&nbsp;</span>
-        {vehicle.model}
+        <span className="lg:sr-only">Anzahl der Mitarbeitenden:&nbsp;</span>
+        {wateringPlan.users.length}
       </p>
 
       <p className="text-dark-800">
-        <span className="lg:sr-only">Benötigte Führerscheinklasse:&nbsp;</span>
-        {vehicle.drivingLicense}
-      </p> */}
+        <span className="lg:sr-only">Anzahl der Bewässerungsgruppen:&nbsp;</span>
+        {wateringPlan.treecluster.length}
+      </p>
     </Link>
   )
 }
