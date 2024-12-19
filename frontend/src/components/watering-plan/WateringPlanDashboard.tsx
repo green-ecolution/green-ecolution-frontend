60f52aa38bc6aff035dda1b1d8165e9f9e051314
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-19 11:28:35 +0100

    feat: create detailed list

--- frontend/src/components/watering-plan/WateringPlanDashboard.tsx
+++ frontend/src/components/watering-plan/WateringPlanDashboard.tsx
@@ -5,25 +5,34 @@ import { wateringPlanIdQuery } from '@/api/queries'
 import { getWateringPlanStatusDetails } from '@/hooks/useDetailsForWateringPlanStatus'
 import { format, formatDistanceToNow } from 'date-fns'
 import { de } from 'date-fns/locale'
+import DetailedList from '../general/DetailedList'
 
 interface WateringPlanDashboardProps {
   wateringPlanId: string
 }
 
-const WateringPlanDashboard = ({ wateringPlanId }: WateringPlanDashboardProps) => {
-  const { data: wateringPlan } = useSuspenseQuery(wateringPlanIdQuery(wateringPlanId))
-  const statusDetails = getWateringPlanStatusDetails(wateringPlan.wateringPlanStatus)
-  const createdDate = wateringPlan?.createdAt
-    ? format(new Date(wateringPlan?.createdAt), 'dd.MM.yyyy')
-    : 'Keine Angabe'
-  const updatedDate = wateringPlan?.createdAt
-    ? formatDistanceToNow(wateringPlan?.updatedAt, { locale: de })
+const WateringPlanDashboard = ({
+  wateringPlanId,
+}: WateringPlanDashboardProps) => {
+  const { data: wateringPlan } = useSuspenseQuery(
+    wateringPlanIdQuery(wateringPlanId)
+  )
+  const statusDetails = getWateringPlanStatusDetails(
+    wateringPlan.wateringPlanStatus
+  )
+  const date = wateringPlan?.date
+  ? format(new Date(wateringPlan?.date), 'dd.MM.yyyy')
+  : 'Keine Angabe'
+  const updatedDate = wateringPlan?.updatedAt
+    ? format(new Date(wateringPlan?.updatedAt), 'dd.MM.yyyy')
     : 'Keine Angabe'
 
   const wateringPlanData = [
     {
       label: 'Länge der Route',
-      value: wateringPlan?.distance ?? 'Keine Angabe',
+      value: wateringPlan?.distance
+        ? `${wateringPlan.distance}`
+        : 'Keine Angabe',
     },
     {
       label: 'Startpunkt',
@@ -31,7 +40,9 @@ const WateringPlanDashboard = ({ wateringPlanId }: WateringPlanDashboardProps) =
     },
     {
       label: 'Benötigte Liter Wasser',
-      value: wateringPlan?.totalWaterRequired ? `${wateringPlan?.totalWaterRequired} Liter` : 'Keine Angabe',
+      value: wateringPlan?.totalWaterRequired
+        ? `${wateringPlan.totalWaterRequired} Liter`
+        : 'Keine Angabe',
     },
     {
       label: 'Transporter',
@@ -43,19 +54,19 @@ const WateringPlanDashboard = ({ wateringPlanId }: WateringPlanDashboardProps) =
     },
     {
       label: 'Bewässerungsgruppen',
-      value: `${wateringPlan?.treecluster.length} Gruppe/n`,
+      value: wateringPlan?.treecluster?.length
+        ? `${wateringPlan.treecluster.length} Gruppe/n`
+        : 'Keine Angabe',
     },
     {
       label: 'Eingeteilte Mitarbeitende',
-      value: `${wateringPlan?.users.length} Mitarbeitende`, // TODO: list names
-    },
-    {
-      label: 'Erstellt',
-      value: createdDate,
+      value: wateringPlan?.users?.length
+        ? `${wateringPlan.users.length} Mitarbeitende`
+        : 'Keine Angabe', // TODO: list names
     },
     {
       label: 'Zuletzt geupdated',
-      value: updatedDate,
+      value: `${updatedDate}`,
     },
   ]
 
@@ -65,30 +76,22 @@ const WateringPlanDashboard = ({ wateringPlanId }: WateringPlanDashboardProps) =
       <article className="space-y-6 2xl:space-y-0 2xl:flex 2xl:items-center 2xl:space-x-10">
         <div className="2xl:w-4/5">
           <h1 className="font-lato font-bold text-3xl mb-4 flex flex-wrap items-center gap-4 lg:text-4xl xl:text-5xl">
-            Einsatzplan: {wateringPlan.date}
-            <Pill label={statusDetails.label} theme={statusDetails?.color ?? 'grey'} />
+            Einsatzplan für den {date}
+            <Pill
+              label={statusDetails.label}
+              theme={statusDetails?.color ?? 'grey'}
+            />
           </h1>
-          {wateringPlan.description && <p className="mb-4">{wateringPlan.description}</p>}
+          {wateringPlan.description && (
+            <p className="mb-4">{wateringPlan.description}</p>
+          )}
         </div>
       </article>
 
-      <section className="mt-16">
-        <h2 className="font-lato font-bold text-2xl mb-4">
-          Daten zur Einsatzplanung
-        </h2>
-        <dl className="text-lg md:columns-2 md:gap-x-11">
-          {wateringPlanData.map((data, index) => (
-            <div
-              key={index}
-              className={`py-4 border-b border-b-dark-200 group md:last:border-b-transparent 
-              ${wateringPlanData.length / 2 === index + 1 ? 'md:border-b-transparent' : ''}`}
-            >
-              <dt className="font-bold sm:inline">{data.label}:</dt>
-              <dd className="sm:inline sm:px-2">{data.value}</dd>
-            </div>
-          ))}
-        </dl>
-      </section>
+      <DetailedList
+        headline="Daten zur Einsatzplanung"
+        details={wateringPlanData}
+      />
     </>
   )
 }
