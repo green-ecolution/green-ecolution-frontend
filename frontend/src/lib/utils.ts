6c752c2b5031382a0cc60d5f955a527204b2199e
Dorien Grönwald <dorien.groenwald@visuellverstehen.de>
2024-12-22 11:59:35 +0100

    chore: delete not needed dependencies

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -1,10 +1,3 @@
-import { type ClassValue, clsx } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
-
 export function decodeJWT<T>(token: string): T {
   const payload = token.split('.')[1]
   const base64 = payload.replace(/-/g, '+').replace(/_/g, '/')
