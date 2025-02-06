fbe13f4c50aa822cf1a88b88e6c1a62889488cf2
Sophia Anthopoulos <sophia.anthopoulos@gmail.com>
2025-02-06 10:23:34 +0100

    fix: console logs and small stuff

--- frontend/src/components/general/form/types/Input.tsx
+++ frontend/src/components/general/form/types/Input.tsx
@@ -2,7 +2,7 @@ import { forwardRef } from "react";
 
 interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   placeholder?: string;
-  label: string;
+  label?: string;
   error?: string;
 }
 
@@ -10,13 +10,16 @@ const Input = forwardRef(
   (props: InputProps, ref: React.ForwardedRef<HTMLInputElement>) => {
     return (
       <div>
-        <label
-          htmlFor={props.name}
-          className="block font-semibold text-dark-800 mb-2.5"
-        >
-          {props.label}{" "}
-          {props.required ? <span className="text-red">*</span> : null}
-        </label>
+        {props.label && (
+          <label
+            htmlFor={props.name}
+            className="block font-semibold text-dark-800 mb-2.5"
+          >
+            {props.label}{" "}
+            {props.required ? <span className="text-red">*</span> : null}
+          </label>
+        )}
+
         <input
           ref={ref}
           id={props.name}
