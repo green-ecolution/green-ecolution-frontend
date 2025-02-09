260ca05faee5b12d11d6944923e61f83d714cd68
Sophia Anthopoulos <sophia.anthopoulos@gmail.com>
2025-02-09 11:29:51 +0100

    fix: added backend values

--- frontend/src/components/map/Map.tsx
+++ frontend/src/components/map/Map.tsx
@@ -1,6 +1,9 @@
 import { MapContainer, TileLayer } from 'react-leaflet'
 import React, { useMemo } from 'react'
 import useMapStore from '@/store/store'
+import { useSuspenseQuery } from '@tanstack/react-query'
+import { infoQuery } from '@/api/queries'
+import L from 'leaflet'
 
 export interface MapProps extends React.PropsWithChildren {
   width?: string
@@ -12,17 +15,17 @@ const Map = ({
   height = 'calc(100dvh - 4.563rem)',
   children,
 }: MapProps) => {
-  const { zoom, boundaries, center, maxZoom, minZoom } = useMapStore(
+  const {data: appInfo } = useSuspenseQuery(infoQuery())
+
+  const { zoom, maxZoom, minZoom } = useMapStore(
     (state) => ({
       zoom: state.map.zoom,
-      boundaries: state.map.boundaries,
-      center: state.map.center,
       maxZoom: state.map.maxZoom,
       minZoom: state.map.minZoom,
     })
   )
+  
   const time = useMemo(() => new Date().getTime(), [])
-    
 
   return (
     <MapContainer
@@ -31,11 +34,14 @@ const Map = ({
       className="z-0"
       zoomControl={false}
       style={{ width, height }}
-      center={center}
+      center={[appInfo?.map?.center.latitude, appInfo.map.center.longitude]}
       zoom={zoom}
       maxZoom={maxZoom}
       minZoom={minZoom}
-      maxBounds={boundaries}
+      maxBounds={L.latLngBounds(
+                [appInfo?.map?.boundsSouthWest.latitude, appInfo.map.boundsSouthWest.longitude],
+                [appInfo?.map?.boundsNorthEast.latitude, appInfo.map.boundsNorthEast.longitude]
+              )}
     >
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- frontend/src/store/map/types.ts
+++ frontend/src/store/map/types.ts
@@ -1,18 +1,13 @@
-import { LatLngBoundsExpression } from "leaflet";
-
 type MapState = {
-  center: [number, number];
-  zoom: number;
-  boundaries: LatLngBoundsExpression
-  minZoom: number;
-  maxZoom: number;
-  showSelectModal: boolean;
-};
+  zoom: number
+  minZoom: number
+  maxZoom: number
+  showSelectModal: boolean
+}
 
 type MapActions = {
-  setCenter: (center: [number, number]) => void;
-  setZoom: (zoom: number) => void;
-  setShowSelectModal: (showSelectModal: boolean) => void;
-};
+  setZoom: (zoom: number) => void
+  setShowSelectModal: (showSelectModal: boolean) => void
+}
 
-export type MapStore = MapState & MapActions;
+export type MapStore = MapState & MapActions
