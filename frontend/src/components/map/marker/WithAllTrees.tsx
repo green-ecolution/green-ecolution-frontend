44addc4537a1aaf32ce03d00a80712daa3f7c733
Cedrik Hoffmann <dev@choffmann.io>
2025-02-19 19:51:58 +0100

    feat: only render tree marker when in bound

--- frontend/src/components/map/marker/WithAllTrees.tsx
+++ frontend/src/components/map/marker/WithAllTrees.tsx
@@ -1,12 +1,23 @@
-import { Tree } from '@green-ecolution/backend-client'
+import { Tree, WateringStatus } from '@green-ecolution/backend-client'
 import { useSuspenseQuery } from '@tanstack/react-query'
 import { treeQuery } from '@/api/queries'
-import TreeMarker from './TreeMarker'
+import L from 'leaflet'
+import { TreeMarkerIcon } from '../MapMarker'
+import { getWateringStatusDetails } from '@/hooks/details/useDetailsForWateringStatus'
+import { useMap } from 'react-leaflet'
+import { useEffect, useRef } from 'react'
 
 export interface WithAllTreesProps {
   onClick?: (tree: Tree) => void
   selectedTrees?: number[]
   hasHighlightedTree?: number
+}
+
+const getStatusColor = (wateringStatus: WateringStatus) => {
+  const statusDetails = getWateringStatusDetails(
+    wateringStatus ?? WateringStatus.WateringStatusUnknown
+  )
+  return statusDetails.colorHex
 }
 
 const WithAllTrees = ({
@@ -15,16 +26,55 @@ const WithAllTrees = ({
   hasHighlightedTree,
 }: WithAllTreesProps) => {
   const { data } = useSuspenseQuery(treeQuery())
+  const map = useMap()
+  const markersRef = useRef<L.Marker<any>[]>([])
 
-  return data.data.map((tree) => (
-    <TreeMarker
-      tree={tree}
-      key={tree.id}
-      onClick={onClick}
-      hasHighlightedTree={hasHighlightedTree}
-      selectedTrees={selectedTrees}
-    />
-  ))
+  useEffect(() => {
+    if (map) {
+      renderMarkers()
+
+      map.on('moveend', renderMarkers)
+    }
+
+    return () => {
+      if (map) {
+        map.off('moveend', renderMarkers)
+        markersRef.current.forEach((m) => map.removeLayer(m))
+      }
+    }
+  }, [map])
+
+  const isSelected = (treeId: number) => {
+    return selectedTrees?.includes(treeId) ?? false
+  }
+
+  const isHighlighted = (treeId: number) => {
+    return hasHighlightedTree === treeId
+  }
+
+  const renderMarkers = () => {
+    if (!map) return;
+
+    const bounds = map.getBounds();
+
+    markersRef.current.forEach((m) => map.removeLayer(m))
+    markersRef.current = [];
+
+    data.data.filter((t: Tree) => bounds.contains([t.latitude, t.longitude])).forEach((t: Tree) => {
+      const icon = TreeMarkerIcon(
+        getStatusColor(t.wateringStatus),
+        isSelected(t.id),
+        isHighlighted(t.id)
+      )
+      const marker = L.marker([t.latitude, t.longitude], { icon })
+      marker.addTo(map)
+      markersRef.current.push(marker)
+
+      marker.on('click', () => onClick?.(t))
+    })
+  }
+
+  return null
 }
 
 export default WithAllTrees
