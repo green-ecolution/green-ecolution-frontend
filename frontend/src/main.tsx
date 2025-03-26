9b8a8b436c1f0921bb5efff3a976a17d61540036
Cedrik Hoffmann <dev@choffmann.io>
2025-03-26 16:50:22 +0100

    feat: add error fallback in router

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -7,9 +7,11 @@ import "@splidejs/react-splide/css";
 import { createRouter, RouterProvider } from "@tanstack/react-router";
 import { routeTree } from "./routeTree.gen";
 import NotFound from "./components/layout/NotFound";
+import ErrorFallback from "./components/layout/ErrorFallback";
 
 const router = createRouter({
   routeTree,
+  defaultErrorComponent: ({ error, reset }) => <ErrorFallback error={error} resetErrorBoundary={reset} />,
   defaultNotFoundComponent: () => {
     return (
       <NotFound />
