18c4f7f976ca6bfba8dddaba153ee1ad18c274bd
Cedrik Hoffmann <dev@choffmann.io>
2025-03-28 10:27:26 +0100

    chore: remove console.log

--- frontend/src/components/layout/Breadcrumb.tsx
+++ frontend/src/components/layout/Breadcrumb.tsx
@@ -4,7 +4,6 @@ import { useBreadcrumbs } from '@/hooks/useBreadcrumb';
 
 function Breadcrumb() {
   const breadcrumbs = useBreadcrumbs();
-  console.log(breadcrumbs)
 
   const rootBreadcrumb = {
     title: 'Dashboard',
