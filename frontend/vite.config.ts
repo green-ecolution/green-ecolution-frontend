0b1ade57ae4e6e0541d3e95d5eb717bd0ca2f684
Cedrik Hoffmann <choffmann@progeek.de>
2025-05-22 10:39:48 +0200

    feat: change vite config to localhost in proxy route /api-local

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -25,7 +25,7 @@ export default defineConfig({
     host: true,
     proxy: {
       '/api-local': {
-        target: 'http://127.0.0.1:3000',
+        target: 'http://localhost:3000',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api-local/, '/api'),
       },
